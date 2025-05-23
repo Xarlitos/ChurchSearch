7e7d090996f90d22ebf62ef5a7a8ed8f3175f076
Xarlitos <04_przekazana.opiekunki@icloud.com>
2025-05-24 01:31:54 +0200

    add filters in upper footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Typography } from '@mui/material';
 import { Libraries, useLoadScript } from "@react-google-maps/api";
-import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import NewMap from "./components/Map";
 import Buttons from "./components/Buttons";
-import UserInfo from "./components/UserInfo.tsx";
 import AboutDialog from "./components/AboutDialog";
 import useGeocode from "./hooks/useGeocode";
 import useNearbySearch from "./hooks/useNearbySearch.ts";
@@ -12,12 +10,8 @@ import useUserLocation from "./hooks/useUserLocation";
 import useMarkers from "./hooks/useMarkers";
 import "./styles/App.css";
 
-interface UserData {
-  name: string;
-  avatarUrl: string;
-}
-
 const LIBRARIES: Libraries = ["places", "marker"];
+// const LIBRARIES: Libraries = ["places", "markers"];
 const MAP_OPTIONS: google.maps.MapOptions = {
   disableDefaultUI: true,
   zoomControl: true,
@@ -27,256 +21,192 @@ const MAP_OPTIONS: google.maps.MapOptions = {
 };
 
 const App: React.FC = () => {
-    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-    if (!apiKey) {
-        console.error("Google Maps API key is missing. Please set VITE_GOOGLE_MAPS_API_KEY in your .env file.");
-        return <div>Error: Missing Google Maps API Key.</div>;
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  console.log("API KEY:", apiKey);
+
+
+  if (!apiKey) {
+    console.error("Google Maps API key is missing. Please set VITE_GOOGLE_MAPS_API_KEY in your .env file.");
+    return <div>Error: Missing Google Maps API Key.</div>;
+  }
+
+  const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer | null>(null);
+  const [center, setCenter] = useState<google.maps.LatLngLiteral>({ lat: 51.9194, lng: 19.1451 });
+  const [showAboutDialog, setShowAboutDialog] = useState<boolean>(false);
+  const [shouldFetchMarkers, setShouldFetchMarkers] = useState<boolean>(true);
+  const mapRef = useRef<google.maps.Map | null>(null);
+  const { searchNearby } = useNearbySearch(mapRef.current);
+  const { geocode } = useGeocode();
+  const { markers, addMarker, clearMarkers } = useMarkers();
+  const [darkMode, setDarkMode] = useState(false);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: apiKey,
+    libraries: LIBRARIES,
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(prev => !prev);
+  };
+
+  const handleGeocode = async (location: string) => {
+    try {
+      const result = await geocode(location);
+      if (result) {
+        const newMarker = {
+          id: Date.now(),
+          name: location,
+          position: result,
+          isFavourite: false,
+        };
+        setCenter(result);
+        addMarker(newMarker);
+        setShouldFetchMarkers(true);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    if (!clientId) {
-        console.error("Google OAuth Client ID is missing. Please set VITE_GOOGLE_OAUTH_CLIENT_ID in your .env file.");
-        return <div>Error: Missing Google OAuth Client ID.</div>;
+  useEffect(() => {
+    if (mapRef.current && shouldFetchMarkers) {
+      searchNearby(center, 5000)
+        .then((places) => {
+          const placeMarkers = places.map((place, index) => {
+            const position = {
+              lat: place.geometry?.location?.lat() ?? 0,
+              lng: place.geometry?.location?.lng() ?? 0,
+            };
+
+            return {
+              id: index,
+              name: place.name || "Unknown Church",
+              position,
+              description: place.types?.join(", ") || "No description",
+              address: place.vicinity || "No address",
+              isFavourite: false,
+            };
+          });
+          clearMarkers();
+          placeMarkers.forEach(addMarker);
+          setShouldFetchMarkers(false);
+          mapRef.current.setZoom(13);
+        })
+        .catch(console.error);
     }
+  }, [center, searchNearby, addMarker, clearMarkers, shouldFetchMarkers]);
 
-    const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer | null>(null);
-    const [user, setUser] = useState<UserData | null>(null);
-    const [center, setCenter] = useState<google.maps.LatLngLiteral>({lat: 51.9194, lng: 19.1451});
-    const [showAboutDialog, setShowAboutDialog] = useState<boolean>(false);
-    const [shouldFetchMarkers, setShouldFetchMarkers] = useState<boolean>(true);
-    const mapRef = useRef<google.maps.Map | null>(null);
-    const {searchNearby} = useNearbySearch(mapRef.current);
-    const {geocode} = useGeocode();
-    const {markers, addMarker, clearMarkers, toggleFavourite, loadFavorites} = useMarkers();
-    const [darkMode, setDarkMode] = useState(false);
-    const {isLoaded} = useLoadScript({
-        googleMapsApiKey: apiKey,
-        libraries: LIBRARIES,
-    });
+  const handleAboutClick = () => {
+    setShowAboutDialog(true);
+  };
 
-    // dark mode app
-    const toggleDarkMode = () => {
-      setDarkMode(prev => !prev);
-    };
+  const handleMarkerClick = (marker: any) => {
+    if (!mapRef.current) return;
 
-    // Handle geocoding and placing markers on the map
-    const handleGeocode = async (location: string) => {
-        try {
-            const result = await geocode(location);
-            if (result) {
-                const newMarker = {
-                    id: Date.now(),
-                    name: location,
-                    position: result,
-                    isFavourite: false,
-                };
-                setCenter(result);
-                addMarker(newMarker);
-                setShouldFetchMarkers(true);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    // Fetch nearby places when map center changes
-    useEffect(() => {
-        if (mapRef.current && shouldFetchMarkers) {
-            searchNearby(center, 5000)
-                .then((places) => {
-                    const placeMarkers = places.map((place, index) => {
-                        const position = {
-                            lat: place.geometry?.location?.lat() ?? 0,
-                            lng: place.geometry?.location?.lng() ?? 0,
-                        };
-
-                        return {
-                            id: index,
-                            name: place.name || "Unknown Church",
-                            position,
-                            description: place.types?.join(", ") || "No description",
-                            address: place.vicinity || "No address",
-                            isFavourite: false,
-                        };
-                    });
-                    clearMarkers();  // Clear previous markers before adding new ones
-                    placeMarkers.forEach(addMarker);
-                    setShouldFetchMarkers(false); // Set to false after fetching markers
-                    mapRef.current.setZoom(13);
-                })
-                .catch(console.error);
-        }
-    }, [center, searchNearby, addMarker, clearMarkers, shouldFetchMarkers]);
-
-    // Handle showing the "About" dialog
-    const handleAboutClick = () => {
-        setShowAboutDialog(true);
-    };
-
-    // Handle clicking on markers to show details
-    const handleMarkerClick = (marker: any) => {
-        if (!mapRef.current) return;
-
-        const { id, name, position, address, isFavourite } = marker;
-        const buttonStyle = `
-        display: flex; align-items: center; gap: 4px; padding: 8px; 
-        border: none; color: white; border-radius: 4px; cursor: pointer;
-        width: 150px;
+    const { id, name, position, address } = marker;
+    const buttonStyle = `
+      display: flex; align-items: center; gap: 4px; padding: 8px; 
+      border: none; color: white; border-radius: 4px; cursor: pointer;
+      width: 150px;
     `;
 
-        const content = user
-            ? `<div style="font-family: Arial, sans-serif; max-width: 300px; padding: 10px;">
-              <h3 style="margin: 0; font-size: 1.2em;">${name}</h3>
-              <p><strong>Coordinates:</strong> ${position.lat.toFixed(6)}, ${position.lng.toFixed(6)}</p>
-              <p><strong>Address:</strong> ${address}</p>
-              <div style="display: flex; flex-direction: column; gap: 8px; margin-top: 8px;">
-                <button id="set-route-${id}" 
-                    style="${buttonStyle} background: #1976d2;">
-                    <img src="route.png" alt="route image" style="width: 20px; height: 20px;" />
-                    <span>Set Route</span>
-                </button>
-                <button id="toggle-favourite-${id}" 
-                    style="${buttonStyle} background: #d32f2f;">
-                    <img src="${isFavourite ? "favourite.png" : "notFavourite.png"}" alt="Heart icon" style="width: 20px; height: 20px;" />
-                    <span>${isFavourite ? "Remove favourite" : "Add favourite"}</span>
-                </button>
-              </div>
-          </div>`
-            : `<div style="font-family: Arial, sans-serif; max-width: 300px; padding: 10px;">
-              <h3 style="margin: 0; font-size: 1.2em;">${name}</h3>
-              <p><strong>Coordinates:</strong> ${position.lat.toFixed(6)}, ${position.lng.toFixed(6)}</p>
-              <p><strong>Address:</strong> ${address}</p>
-              <p style="color: red;">Log in to add to favourites.</p>
-              <div style="display: flex; flex-direction: column; gap: 8px; margin-top: 8px;">
-                <button id="set-route-${id}" 
-                    style="${buttonStyle} background: #1976d2;">
-                    <img src="route.png" alt="route image" style="width: 20px; height: 20px;" />
-                    <span>Set Route</span>
-                </button>
-              </div>
-          </div>`;
+    const content = `
+      <div style="font-family: Arial, sans-serif; max-width: 300px; padding: 10px;">
+        <h3 style="margin: 0; font-size: 1.2em;">${name}</h3>
+        <p><strong>Coordinates:</strong> ${position.lat.toFixed(6)}, ${position.lng.toFixed(6)}</p>
+        <p><strong>Address:</strong> ${address}</p>
+        <div style="display: flex; flex-direction: column; gap: 8px; margin-top: 8px;">
+          <button id="set-route-${id}" 
+              style="${buttonStyle} background: #1976d2;">
+              <img src="route.png" alt="route image" style="width: 20px; height: 20px;" />
+              <span>Set Route</span>
+          </button>
+        </div>
+      </div>`;
 
-        const infoWindow = new google.maps.InfoWindow({
-            content,
-            position,
-        });
+    const infoWindow = new google.maps.InfoWindow({
+      content,
+      position,
+    });
 
-        infoWindow.open(mapRef.current);
+    infoWindow.open(mapRef.current);
 
-        google.maps.event.addListenerOnce(infoWindow, "domready", () => {
-            const button = document.getElementById(`toggle-favourite-${id}`);
-            if (button) {
-                button.addEventListener("click", () => toggleFavourite(marker));
-            }
-        })
+    google.maps.event.addListenerOnce(infoWindow, "domready", () => {
+      const button = document.getElementById(`set-route-${id}`);
+      if (button) {
+        button.addEventListener("click", () => setRoute(position));
+      }
+    });
+  };
 
-        google.maps.event.addListenerOnce(infoWindow, "domready", () => {
-            const button = document.getElementById(`set-route-${id}`);
-            if (button) {
-                button.addEventListener("click", () => setRoute(position));
-            }
-        });
-    };
-
-    const setRoute = (destination: google.maps.LatLngLiteral) => {
-        if (!mapRef.current || !userPosition) {
-            console.error("Map or user position is not available.");
-            return;
-        }
-
-        // Jeśli istnieje aktywna trasa, usuń ją
-        if (directionsRenderer) {
-            directionsRenderer.setMap(null); // Usuń poprzednią trasę z mapy
-        }
-
-        const newDirectionsRenderer = new google.maps.DirectionsRenderer();
-        const directionsService = new google.maps.DirectionsService();
-
-        newDirectionsRenderer.setMap(mapRef.current);
-        setDirectionsRenderer(newDirectionsRenderer);
-
-        directionsService.route(
-            {
-                origin: userPosition,
-                destination,
-                travelMode: google.maps.TravelMode.DRIVING,
-            },
-            (result, status) => {
-                if (status === google.maps.DirectionsStatus.OK) {
-                    newDirectionsRenderer.setDirections(result);
-                } else {
-                    console.error(`Directions request failed due to ${status}`);
-                }
-            }
-        );
-    };
-
-    // Handle Google login success
-    const handleSuccess = (response: CredentialResponse) => {
-        console.log("Logged in successfully!", response.credential);
-
-        const userData: UserData = {
-            name: "John Doe",
-            avatarUrl: 'https://example.com/avatar.jpg',
-        };
-
-        setUser(userData);
-        loadFavorites();
-    };
-
-    const handleError = () => {
-        console.error("Login failed");
-    };
-
-    const handleLogout = () => {
-        setUser(null);
-    };
-
-    // Fetch user location on mount
-    const {userPosition, fetchUserLocation} = useUserLocation();
-    useEffect(() => {
-        if (isLoaded) {
-            fetchUserLocation();
-        }
-    }, [fetchUserLocation, isLoaded]);
-
-    // Clear all markers
-    const handleClearMarkers = () => {
-        clearMarkers();
-        setShouldFetchMarkers(false);
-        clearRoutes()
-    };
-
-    const clearRoutes = () => {
-        directionsRenderer?.setMap(null);
+  const setRoute = (destination: google.maps.LatLngLiteral) => {
+    if (!mapRef.current || !userPosition) {
+      console.error("Map or user position is not available.");
+      return;
     }
 
+    if (directionsRenderer) {
+      directionsRenderer.setMap(null);
+    }
 
-    const handleNavigate = async (destinationAddress: string) => {
-        if (destinationAddress && mapRef.current && userPosition) {
-            try {
-                const result = await geocode(destinationAddress); // Użyj funkcji geokodowania do pobrania współrzędnych adresu
-                if (result) {
-                    // Wywołaj funkcję nawigacyjną
-                    setRoute(result);
-                } else {
-                    console.error("Couldn't geocode the address.");
-                }
-            } 
-            catch (error) {
-                console.error("Geocoding error:", error);
-            }
-        } 
-        else {
-            console.error("Address or user location is not available.");
+    const newDirectionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
+
+    newDirectionsRenderer.setMap(mapRef.current);
+    setDirectionsRenderer(newDirectionsRenderer);
+
+    directionsService.route(
+      {
+        origin: userPosition,
+        destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          newDirectionsRenderer.setDirections(result);
+        } else {
+          console.error(`Directions request failed due to ${status}`);
         }
-    };
+      }
+    );
+  };
 
+  const { userPosition, fetchUserLocation } = useUserLocation();
+  useEffect(() => {
+    if (isLoaded) {
+      fetchUserLocation();
+    }
+  }, [fetchUserLocation, isLoaded]);
 
-  // Handle user clicking "My Location"
+  const handleClearMarkers = () => {
+    clearMarkers();
+    setShouldFetchMarkers(false);
+    clearRoutes();
+  };
+
+  const clearRoutes = () => {
+    directionsRenderer?.setMap(null);
+  };
+
+  const handleNavigate = async (destinationAddress: string) => {
+    if (destinationAddress && mapRef.current && userPosition) {
+      try {
+        const result = await geocode(destinationAddress);
+        if (result) {
+          setRoute(result);
+        } else {
+          console.error("Couldn't geocode the address.");
+        }
+      } catch (error) {
+        console.error("Geocoding error:", error);
+      }
+    } else {
+      console.error("Address or user location is not available.");
+    }
+  };
+
   const handleMyLocation = () => {
     if (mapRef.current && userPosition) {
-      setCenter(userPosition); // Update the center of the map to user's location
+      setCenter(userPosition);
       mapRef.current.setZoom(18);
     } else {
       console.error("User location is not available");
@@ -301,14 +231,8 @@ const App: React.FC = () => {
             setDarkMode={setDarkMode}
           />
         </div>
-        <div className="google-login-container">
-          {!user ? (
-            <GoogleLogin onSuccess={handleSuccess} onError={handleError} useOneTap />
-          ) : (
-            <UserInfo name={user.name} avatarUrl={"https://example.com/avatar.jpg"} onLogout={handleLogout} />
-          )}
-        </div>
       </div>
+
       <div className="map-container">
         <NewMap
           center={center}
@@ -317,33 +241,29 @@ const App: React.FC = () => {
           userPosition={userPosition}
           mapRef={(map) => (mapRef.current = map)}
           onClickMarker={handleMarkerClick}
-          darkMode={darkMode}  // Dodanie darkMode jako prop
+          darkMode={darkMode}
         />
       </div>
 
-  
       <div className="bottom-footer">
-          <Typography 
-            variant="body2" 
-            color={darkMode ? "white" : "textSecondary"} 
-            align="center"
-          >
+        <Typography 
+          variant="body2" 
+          color={darkMode ? "white" : "textSecondary"} 
+          align="center"
+        >
           Church Locator v0.9.1
         </Typography>
       </div>
-  
-       {/* Przekazanie darkMode do AboutDialog */}
+
       {showAboutDialog && (
         <AboutDialog 
           open={showAboutDialog} 
           onClose={() => setShowAboutDialog(false)} 
-          darkMode={darkMode}  // Przekazanie darkMode do AboutDialog
+          darkMode={darkMode}
         />
       )}
     </div>
   );
-  
-  
 };
 
 export default App;
